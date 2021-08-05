6d082b0e8bdb357faeccddbedcef8426cf5c7a22
My Name <alex971568906558@gmail.com>
2021-08-05 17:31:48 +0300

    1

---
+++ main.py
@@ -0,0 +1,57 @@
+import telebot
+from telebot import types
+from config import token
+
+
+#Vigbo_HR_bot
+
+bot = telebot.TeleBot(token)
+
+
+name = str
+#how_are_you = str
+#age = int
+
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    #bot.send_message(message.chat.id, "Привет! Давай познакомимся! Как тебя зовут?")
+    bot.send_message(message.from_user.id, 'Привет, давай познакомимся! Как тебя зовут?')
+    bot.register_next_step_handler(message, reg_name)
+
+
+def reg_name(message):
+    global name
+    name = message.text
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    markup.row('Прекрасно', 'Нормально')
+    #markup.row('Нормально')
+    markup.row('Так себе', 'Болею')
+    #markup.row('Болею')
+    markup.row('Хочу в отпуск!!!')
+    bot.send_message(message.from_user.id, 'как ты себя чувствуешь на этой неделе?', reply_markup=markup)
+
+
+
+@bot.message_handler(content_types=['text'])
+def text(message):
+    if message.text == 'Прекрасно':
+        bot.send_message(message.chat.id, "Супир! Давай же скорее сделаем VIGBO самым крутым продуктом!")
+    elif message.text == 'Нормально':
+        bot.send_message(message.chat.id, "Мотивирующий текст")
+    elif message.text == 'Так себе':
+        bot.send_message(message.chat.id,  "Напиши эйчару @Ksenya_zh, она поможет")
+    elif message.text == 'Болею':
+        bot.send_message(message.chat.id, "ссылка на регламент sick days, http://www.ems.by/")
+    elif message.text == 'Хочу в отпуск!!!':
+        bot.send_message(message.chat.id, "ссылка на статью про work-life и на регламент согласования отпуска")
+
+
+
+
+
+
+
+
+bot.polling(none_stop=True)
